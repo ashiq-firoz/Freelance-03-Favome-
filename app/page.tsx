5ea796072fcf7b52be5cc84f3d14beaa8c6bc49e
ashiq-firoz <ashiqfiroz08@gmail.com>
2024-07-05 11:21:05 +0530

    new hero

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import { SparklesPreview } from "./components/Hero";
 import About from "./components/About";
 import Products from "./components/Products";
 import { SignupFormDemo } from "./components/Contact";
+import Hero from "./components/Hero3";
 
 export default function Page() {
   return (
    <>
    {/* <BackgroundBeamsDemo/> */}
-   <SparklesPreview/>
+   <Hero/>
    <About/>
    <img src="/img/banner.jpg" className="w-max" alt="" />
    <Products/>
