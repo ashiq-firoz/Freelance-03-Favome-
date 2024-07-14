91b0dc74421dd0077f01456a06d15c2a2c91d4a6
ashiq-firoz <ashiqfiroz08@gmail.com>
2024-07-14 21:26:00 +0530

    build update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,42 +2,36 @@
 
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { Antonio } from 'next/font/google'
+import { Antonio } from 'next/font/google';
 
 export const antonio = Antonio({
   subsets: ['latin'],
   display: 'swap',
-  weight : '400'
+  weight: '400',
 });
 
 const Hero: React.FC = () => {
     const initialCards: Record<string, JSX.Element> = {
         "1": (
-            <>
-                <img
-                    src="/img/Card.jpeg"
-                    alt="Favome Card"
-                    className="pointer-events-none mx-auto  lg:h-[450px] rounded-2xl border-2 border-slate-700 object-cover backdrop-blur-md backdrop-filter"
-                />
-            </>
+            <img
+                src="/img/Card.jpeg"
+                alt="Favome Card"
+                className="pointer-events-none mx-auto lg:h-[450px] rounded-2xl border-2 border-slate-700 object-cover backdrop-blur-md backdrop-filter"
+            />
         ),
         "2": (
-            <>
-                <img
-                    src="/img/Card3.jpg"
-                    alt="Favome Card"
-                    className="pointer-events-none  mx-auto h-[450px] w-[350px] border-2 border-slate-700 bg-slate-200 object-cover rounded-2xl"
-                />
-            </>
+            <img
+                src="/img/Card3.jpg"
+                alt="Favome Card"
+                className="pointer-events-none mx-auto h-[450px] w-[350px] border-2 border-slate-700 bg-slate-200 object-cover rounded-2xl"
+            />
         ),
         "3": (
-            <>
-                <img
-                    src="/img/Card2.jpeg"
-                    alt="Favome Card"
-                    className="pointer-events-none  mx-auto h-[450px] w-auto border-2 border-slate-700 bg-slate-200 object-cover rounded-2xl"
-                />
-            </>
+            <img
+                src="/img/Card2.jpeg"
+                alt="Favome Card"
+                className="pointer-events-none mx-auto h-[450px] w-auto border-2 border-slate-700 bg-slate-200 object-cover rounded-2xl"
+            />
         ),
     };
 
@@ -77,22 +71,22 @@ const Hero: React.FC = () => {
         }, 500); // 500 milliseconds = 0.5 seconds
     };
 
-    
-
     return (
         <section className="overflow-hidden h-full bg-slate-900 px-8 py-24 text-slate-50">
             <div className="mx-auto grid max-w-6xl grid-cols-1 items-center gap-16 md:grid-cols-2 md:gap-52">
                 <div className='py-10'>
-
-                    <h3 className={` text-7xl lg:text-9xl mt-16 lg:mt-28 font-black leading-[1.25] md:text-7xl mr-[100px]`}>
-                       <span className={`${antonio.className}`}>FAVOME</span><sup>&trade;</sup>
-                    </h3>
-                    <p className={`${antonio.className} mb-8  text-xl lg:text-2xl text-slate-300 `}>
-                        MY FAVORITE WAY
-                    </p>
-    
-                    <p className="mb-8 mt-4 text-lg lg:text-xl text-slate-400 ">
-                        Digitize your business, education, entertainment, career, and personal life with FAVOME. <br></br>
+                    <center>
+                        <img src="/img/logo.png" className='h-16 justify-center' alt="" />
+                        <h3 className={`text-7xl lg:text-9xl font-black leading-[1.25] md:text-7xl mr-[100px] flex items-center`}>
+                            <span className={`${antonio.className} ml-12 lg:ml-0`}>&nbsp;&nbsp;FAVOME</span>
+                            <img src="/img/tm_bg.png" className='h-7 lg:h-8 ml-4 mb-8 lg:mb-12' alt="Trademark" />
+                        </h3>
+                        <p className={`${antonio.className} mb-8 text-xl lg:text-2xl text-slate-300`}>
+                            MY FAVORITE WAY
+                        </p>
+                    </center>
+                    <p className="mb-8 mt-4 text-lg lg:text-xl text-slate-400">
+                        Digitize your business, education, entertainment, career, and personal life with FAVOME. <br />
                         Unlock your unlimited potential today
                     </p>
                 </div>
